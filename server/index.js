a56d84eafdc3df8c5c254dd1e9a8862840c5bc41
Arttu Saarinen <arttusaarinen95@gmail.com>
2019-10-10 16:59:20 +0800

    Authentication, folder refactoring

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const app = express();
+require('./services/passport.js');
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+const app = express();
+require('./routes/authRoutes.js')(app);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT) 
