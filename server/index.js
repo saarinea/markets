e2a05885860490cccbc678b1cab30c510cf1fd24
Arttu Saarinen <arttusaarinen95@gmail.com>
2019-10-11 18:47:08 +0800

    Database connection, user model creation/init

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport.js');
+require('./models/User.js')
+const keys = require('./config/keys.js')
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 require('./routes/authRoutes.js')(app);
