4b2cf61b17d178c9a1846ba4fa26f986465f937f
Arttu Saarinen <arttusaarinen95@gmail.com>
2019-10-11 23:49:58 +0800

    Authentication flow completed

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('./services/passport.js');
-require('./models/User.js')
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys.js')
+require('./models/User.js');
+require('./services/passport.js');
+
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes.js')(app);
 
 const PORT = process.env.PORT || 5000
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,15 +1,40 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys.js')
+const mongoose = require('mongoose')
+const keys = require('../config/keys.js');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then(user => {
+        done(null, user);
+    });
+});
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refrestoken', refreshToken);
-        console.log('profile', profile);
+        User.findOne({googleID: profile.id})
+        .then((existingUser) => {
+            if(existingUser) {
+                done(null, existingUser);
+            } else{
+                new User({ 
+                    googleID: profile.id,
+                    name: profile.name.givenName
+                })
+                .save()
+                .then(user => done(null, user))
+
+            }
+        })
     }
     )
 );
