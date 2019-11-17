0b44d7951df07e9aeea70f363bdefdfb11db5754
Arttu Saarinen <arttusaarinen95@gmail.com>
2019-11-17 18:03:06 +0800

    Refactoring async await

--- server/services/passport.js
+++ server/services/passport.js
@@ -21,21 +21,19 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({googleID: profile.id})
-        .then((existingUser) => {
-            if(existingUser) {
-                done(null, existingUser);
-            } else{
-                new User({ 
-                    googleID: profile.id,
-                    name: profile.name.givenName
-                })
-                .save()
-                .then(user => done(null, user))
-
-            }
-        })
+    }, 
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({googleID: profile.id})
+            
+        if (existingUser) {
+            return done(null, existingUser);
+        } 
+        
+        const user = await new User({ 
+            googleID: profile.id,
+            name: profile.name.givenName
+        }).save()
+        done(null, user)
     }
     )
 );
