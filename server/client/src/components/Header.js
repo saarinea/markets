29dc016c4f42028ed5322930cd854666a70e1105
Arttu Saarinen <arttusaarinen95@gmail.com>
2019-11-19 00:14:44 +0800

    Front end dev, header, reducers

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">
+                    <a className="left brand-logo" href="/">
                         Markets
                     </a>
                     <ul className="right">
