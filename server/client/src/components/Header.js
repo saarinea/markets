21ae9922c5df9dba592a5ec9e469f2bd60315acb
Arttu Saarinen <arttusaarinen95@gmail.com>
2019-11-20 01:29:09 +0800

    Header development, login/logout functionality

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,22 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
-    render() {
-        return (
-            <nav>
-                <div className="nav-wrapper">
-                    <a className="left brand-logo" href="/">
-                        Markets
-                    </a>
-                    <ul className="right">
-                        <li>
-                            <a>Login with Google</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        )
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>
+      default: 
+        return <li><a href="/api/logout">Logout</a></li>
+
     }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo" href="/">
+            Markets
+          </a>
+          <ul className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
