1beafa75e71923430cc43cee69c06f70c7ad514a
Arttu Saarinen <arttu.saarinen@tuni.fi>
2020-03-10 13:48:40 +0200

    Alphavantage API development, show example data

--- server/client/src/components/Stocks.js
+++ server/client/src/components/Stocks.js
@@ -1,13 +1,48 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 
 class Stocks extends Component {
+    state = {
+      error: null,
+      isLoaded: false,
+      timeseries: []
+    }
+
+    componentDidMount() {
+        fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo')
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({ 
+              timeseries: JSON.stringify(data),
+              isLoaded: true 
+            })
+            console.log(JSON.stringify(this.state.timeseries))
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        })
+        .catch(console.log())
+    }
 
   render() {
-    return (
-      <h2>Stocks</h2>
-    );
+    const {error, isLoaded, timeseries} = this.state;
+
+    if (error){
+      return <div>Error in loading</div>
+    }
+    else if(!isLoaded){
+      return <div>Loading...</div>
+    }
+    else{
+      return (
+        //<h2>Stocks</h2>
+        <div> {timeseries} </div>
+      );
+    }
+
+
   }
 }
 
