674f0e7ae92658ff366f166637c8f140d8c60ffe
Arttu Saarinen <arttu.saarinen@tuni.fi>
2020-03-11 18:51:57 +0200

    Header formatting, .css file created

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,29 +3,39 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 class Header extends Component {
-  renderContent() {
+  renderLoginButton() {
     switch (this.props.auth) {
       case null:
         return;
       case false:
-        return <li><a href="/auth/google">Login With Google</a></li>
-      default: 
-        return <li><a href="/api/logout">Logout</a></li>
-
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
     }
   }
 
   render() {
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <Link 
-          to={this.props.auth ? '/stocks' : '/'} 
-          className="left brand-logo" href="/">
-            Markets
-          </Link>
-          <ul className="right">
-            {this.renderContent()}
+      <nav class="navbar navbar-default navbar-fixed-top">
+        <div id="navbar container">
+          <ul class="nav navbar-nav header">
+            <li>
+              <Link
+                to={this.props.auth ? "/stocks" : "/"}
+                href="/"
+              >
+                Markets
+              </Link>
+            </li>
+            <li class="right">{this.renderLoginButton()}</li>
           </ul>
         </div>
       </nav>
