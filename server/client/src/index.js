483a7e5a61fcd9105fca3e04f83c348675c61b05
Arttu Saarinen <arttu.saarinen@tuni.fi>
2020-03-31 16:02:58 +0300

    Styles-directory for stylesheets created

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,7 +2,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import './components/index.css'
+import './styles/index.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; 
