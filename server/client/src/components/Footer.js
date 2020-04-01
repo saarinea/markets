2f2f2dfaea70ff8a852c225eb834dc3c0a9c6f0a
Arttu Saarinen <arttu.saarinen@tuni.fi>
2020-04-01 11:24:52 +0300

    Footer modification

--- server/client/src/components/Footer.js
+++ server/client/src/components/Footer.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
     return (
       <footer className="py-3 footer-custom">
         <div className="container text-center">
-          &copy; Arttu Saarinen
+          <a href="https://www.linkedin.com/in/arttu-saarinen-35844b135/">
+            &copy; Arttu Saarinen
+          </a>
         </div>
       </footer>
-    )
+    );
   }
 }
 
-export default Footer
+export default Footer;
